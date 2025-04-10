c5e03b78caaae76ee8f881433bdacf6b7c6699a4
Yzung <wagnner_carlesso@outlook.com.br>
2025-04-10 17:45:51 -0300

    EZ Pilot

---
+++ Easyhome/src/main/java/EZHome/com/Dom-C/EasyHomeApplication.java
@@ -0,0 +1,13 @@
+package EZHome.com.Dom.C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyHomeApplication.class, args);
+	}
+
+}
---
+++ Easyhome/src/test/java/EZHome/com/Dom/C/EasyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package EZHome.com.Dom.C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
